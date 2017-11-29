219422987f87733a1106735264bbe03a5ec4eb2d
Marc Fournier <marc.fournier91@gmail.com>
2017-11-29 00:09:32 -0500

    Jello world

---
+++ app/index.js
@@ -0,0 +1,5 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+var App = require('../components/app')
+
+ReactDOM.render(<App name="Marc" />, document.getElementById('root'))
