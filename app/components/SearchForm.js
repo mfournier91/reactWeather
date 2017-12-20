870c0de64043e8fadb141bcba5e0105f56603c96
Marc Fournier <marc.fournier91@gmail.com>
2017-12-19 22:19:01 -0500

    get requests to api logged to console

--- app/components/SearchForm.js
+++ app/components/SearchForm.js
@@ -1,12 +1,32 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const Axios = require('axios');
+const secretKey = require('./Config').apiKey;
 
 class SearchForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            cityState: ""
+        }
+    }
+    onClickHandler = (e) => {
+        e.preventDefault();
+        console.log('clicked ' + this.state.cityState);
+        Axios.get('http://api.openweathermap.org/data/2.5/weather?q=' + this.state.cityState + '&type=accurate&APPID=' + secretKey).then((res) => {
+                console.log(res)
+        }).catch((err)=> {
+            console.log(err)
+        })
+    }
+    onChangeHandler = (event) => {
+        this.setState({cityState: event.target.value})
+    }
     render() {
         return (
-            <div className="searchContainer" style={{flexDirection: this.props.direction}}>
+            <div className="searchContainer" onChange={this.onChangeHandler} value={this.state.cityState} style={{flexDirection: this.props.direction}}>
                 <input type="text" className="form-control" placeholder="College Park, Maryland"/>
-                <button className="btn btn-success">Get Weather</button>
+                <button onClick={this.onClickHandler} className="btn btn-success">Get Weather</button>
             </div>
 
         );
