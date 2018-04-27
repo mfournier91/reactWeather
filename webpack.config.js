8a6510ccd126630c359275b5db2600900142eb6b
Marc Fournier <marc.fournier91@gmail.com>
2018-04-27 00:00:15 -0400

    Set up script for production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './app/index.html',
   filename: 'index.html',
   inject: 'body'
 })
 
-module.exports = {
+var config = {
   entry: './app/index.js',
   output: {
     path: path.resolve('dist'),
@@ -27,3 +29,17 @@ module.exports = {
     },
     plugins: [HtmlWebpackPluginConfig]
 }
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+      new webpack.DefinePlugin({
+          'process.ENV' : {
+            'NODE_ENV' : JSON.stringify(process.env.NODE_ENV)
+          }
+      }),
+      new webpack.optimize.UglifyJsPlugin()
+  )
+}
+
+
+module.exports = config;
